b750e551323d84bd12fd13f6bc5f7410b03469c9
Noah Beamen <mobileweb1989@hotmail.com>
2016-03-29 04:13:51 +0200

    Update db_conn.php

--- todo_table/include/db_conn.php
+++ todo_table/include/db_conn.php
@@ -5,11 +5,6 @@ $db_user = "root";
 $db_pass = "";
 $db_name="jtabletestdb";
 
-//$db_host = "localhost";
-//$db_user = "newteci3_jin1989";
-//$db_pass = "kgj1011989";
-//$db_name="newteci3_jtabletestdb";
-
 $con = mysql_connect($db_host,$db_user,$db_pass);
 mysql_select_db($db_name, $con);
 ?>
